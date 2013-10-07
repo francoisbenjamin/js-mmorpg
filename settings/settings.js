11d02317f15e92f3731261f4e987ba04e49af0ce
Shinochi <francois.ben01@gmail.com>
2013-10-07 17:34:32 +0200

    Added a setting file for the server

---
+++ settings/settings.js
@@ -0,0 +1,14 @@
+/**
+* The settings of the server
+*/
+var settings = {
+		db : "js-mmorpg",
+		// Default mongo port
+		db_port : 27017,
+		// Private crypting key
+		// You'll need to change it
+		secret : "_js-mm0rpg_1s_4mazing_",
+		port : 5000
+};
+
+exports.settings = settings;
